a4e904381a7544906f94f51971059eaa53a5cdae
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2013-09-22 12:39:17 -0400

    Added interpretation of coordinate range

--- gplot.py
+++ gplot.py
@@ -4,6 +4,8 @@ from netCDF4 import Dataset
 import argparse
 import re
 
+debug = False # Global debugging
+
 def doTheThing(fileName, variableName, sliceSpecs):
   # Open netcdf file
   try: rg=Dataset( fileName, 'r' );
@@ -30,57 +32,97 @@ def doTheThing(fileName, variableName, sliceSpecs):
   # dimension spec for relevence. If any named spec matches the dim, apply it. If not,
   # apply the first non-specific spec. If there are no general spec's left, use the full
   # dimension.
-  print 'sl',sliceSpecs
+  if debug: print 'All slice specifications=',sliceSpecs
   reGen = re.compile('[a-zA-Z_]')
   generalSpecs = [s for s in sliceSpecs for m in [reGen.match(s)] if not m]
-  print 'generalSpecs=',generalSpecs
+  if debug: print 'All general specifications=',generalSpecs
   slices = []
-  dims = var.dimensions
+  dims = var.dimensions; vars = rg.variables
   for dim in dims:
-    print 'Processing',dim,' sliceSpecs=',sliceSpecs
-    if len(sliceSpecs)>0:
+    if debug: print 'Processing',dim,' slice specs=',sliceSpecs
+    if len(sliceSpecs)>0: # Look for user provided spec
       reDim = re.compile(dim+'=', re.IGNORECASE)
       matchingSliceSpec = [s for s in sliceSpecs for m in [reDim.match(s)] if m]
-      print 'ms=',matchingSliceSpec
+      if debug: print '  Matching spec=',matchingSliceSpec
+      # More than one match is an error
       if len(matchingSliceSpec)>1: error('Only one specification per dimension is allowed.\n'
           'The following specs appear to be for the same dimension: '+str(matchingSliceSpec))
       elif len(matchingSliceSpec)==0: # No spec is specific to this dimesion
-        if len(generalSpecs):
+        if len(generalSpecs): # Use up a general spec if any
           matchingSliceSpec = generalSpecs[0]
           generalSpecs.remove(matchingSliceSpec)
-          print 'generalSpecs=',generalSpecs
+          if debug: print '  New general specs=',generalSpecs
           sliceSpecs.remove(matchingSliceSpec) # Pop this spec from the stack
-      elif len(matchingSliceSpec)==1:
+        else: matchingSliceSpec = ':' # if no general specs left
+      elif len(matchingSliceSpec)==1: # There is one matching specific spec
         matchingSliceSpec=matchingSliceSpec[0]
         sliceSpecs.remove(matchingSliceSpec) # Pop this spec from the stack
     else: matchingSliceSpec = ':' # Stack was empty
-    slices += [ iRange(rg.dimensions[dim], str(matchingSliceSpec)) ]
+    if dim in vars: dVar = rg.variables[dim]
+    else: dVar = None
+    slices += [ iRange(rg.dimensions[dim], str(matchingSliceSpec), dVar) ]
+   #try: slices += [ iRange(rg.dimensions[dim], str(matchingSliceSpec), var) ]
+   #except: error('Unable to interpret dimension specificion "'+str(matchingSliceSpec)+'".')
+  # Check that all user provided specs were used
   if len(sliceSpecs)>0: error('Some dimension specifications were not used.\n'
     +'Specifically, specifications '+''.join(str(s)+' ' for s in sliceSpecs)+' were unusable.\n'
     +'Variable "'+variableName+'" has dimensions: '+''.join(str(d)+' ' for d in dims)+'\n')
-  print 'slices=',slices
+  if debug: print 'slices=',slices
 
-def iRange(ncDim, strSpec): # Interpret strSpec and return indices
-  parts = strSpec.split('='); dLen = len(ncDim)
-  print 'parts=',parts
-  if len(parts)>2: error('Syntax error in "'+strSpec+'".')
-  elif len(parts)==0: error('Impossible!')
-  elif len(parts)==1: # Format : is: :ie is:ie
-    iParts = parts[0].split(':')
-    print 'iParts=',iParts
-    if iParts[0]=='': indS = 0
-    else: indS = int(iParts[0])-1 # Convert fortran/matlab index to python
-    if len(iParts)==1: indE = indS+1
-    elif iParts[1]=='': indE = dLen
-    else: indE = int(iParts[1]) # No conversion necessary because of python ranges
+  # Now read the data
+  data = var[slices]
+  print data
+
+def iRange(ncDim, strSpec, ncVar): # Interpret strSpec and return list of indices
+  equalParts = strSpec.split('='); dLen = len(ncDim)
+  if debug: print '    = split',equalParts
+  if len(equalParts)>2: error('Syntax error in "'+strSpec+'".')
+  elif len(equalParts)==0: error('Impossible!')
+  elif len(equalParts)==1: # Format : is: :ie is:ie
+    colonParts = equalParts[0].split(':')
+    if debug: print '    : split',colonParts
+    if len(colonParts)>2: error('Too many :\'s in "'+strSpec+'".')
+    # Interpret left of : to set indS
+    if colonParts[0]=='': indS = 0
+    else: indS = int(colonParts[0])-1 # Convert fortran/matlab index to python
+    # Interpret right of : to set indE
+    if len(colonParts)==1: indE = indS+1
+    elif colonParts[1]=='': indE = dLen
+    else: indE = int(colonParts[1]) # No conversion necessary because of python ranges
     if indS>=indE: error('In '+strSpec+' the index range is reversed.\n')
     if indS<0: error('In '+strSpec+' the start of the index range must >=1.\n')
     if indE>dLen: error('In '+strSpec+' the end of the index range is out of bounds.\n'
         +'The corresponding dimension, '+str(ncDim._name)+', has length '+str(dLen)+'.')
-  elif len(parts)==2:
-    error('Not yet implemented!')
-  if indE-indS==1: return indS
-  else: return [indS,indE]
+  elif len(equalParts)==2: # Format =: =xs: =:xe =xs:xe
+    coordVals = ncVar[:]; coordSign = 1
+    if coordVals[-1] < coordVals[0]: coordSign = -1 # In lieu of a sign() function
+    colonParts = equalParts[1].split(':')
+    if debug: print '    : split',colonParts
+    if len(colonParts)==1 and colonParts[0]=='': error('Nothing on the r.h.s. of =\'s!')
+    if len(colonParts)>2: error('Too many :\'s in "'+strSpec+'".')
+    # Interpret left of : to set coordS
+    if colonParts[0]=='': coordS = coordVals[0]
+    else: coordS = float(colonParts[0])
+    # Interpret right of : to set coordE
+    if len(colonParts)==1: coordE = coordS
+    elif colonParts[1]=='': coordE = coordVals[-1]
+    else: coordE = float(colonParts[1])
+    if debug: print '    coord range=',coordS,coordE
+    if coordSign*(coordE-coordS) < 0: error('The coordinate range "'+strSpec+'" is inverted!')
+    indS = coord2index(coordVals, coordS, roundUp=True)
+    if coordE==coordS: indE = indS + 1
+    else: indE = coord2index(coordVals, coordE)+1 # Python range requires +1
+  if debug: print '    is,ie=',indS,indE
+  if indE-indS==1: return [indS]
+  else: return range(indS,indE)
+
+def coord2index( coordList, coordVal, roundUp=False): # Returns index of nearest coordList
+  ind = min(range(len(coordList)), key=lambda i:abs(coordList[i]-coordVal))
+  if roundUp and (ind+1<len(coordList)):
+    # Correct for roundin 0.5 down to 0.
+    if abs(coordList[ind+1]-coordVal)<=abs(coordList[ind]-coordVal): ind=ind+1
+  if debug: print '      coord(',ind,')=',coordList[ind],' matches coord=',coordVal
+  return ind
 
 def summarizeFile(rg):
   vars = rg.variables
@@ -108,10 +150,11 @@ def summarizeFile(rg):
     print oString
 
 def error(msg,code=9):
-  print 'Error: ' + msg + ' Aborting!'
+  print 'Error: ' + msg
   exit(code)
 
 def main():
+  global debug # Declared global in order to set it
 
   # Arguments
   parser = argparse.ArgumentParser(description=
@@ -125,7 +168,11 @@ def main():
   parser.add_argument('pos', type=str,
                       nargs='*', default='',
                       help='Indices or location specification.')
+  parser.add_argument('-d','--debug', action='store_true',
+                      help='Turn on debugging information.')
   args = parser.parse_args()
+
+  if args.debug: debug = True
 
   doTheThing(args.filename, args.variable, args.pos)
 
